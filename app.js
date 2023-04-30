9a2aceef61ba05485086bc84f314446fafa4fa3b
Irina Mychko <nuorri@gmail.com>
2023-04-30 19:09:57 +0300

    feat: created keyboard class js

---
+++ app.js
@@ -0,0 +1,42 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+
+    },
+
+    _createKeys() {
+
+    },
+
+    _triggerEvent(handlerName) {
+        console.log("Event Triggered! Event Name: " + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("Caps Lock Toggled!");
+    },
+
+    open(initialValue, oninput,onclose) {
+
+    },
+
+    close() {
+
+    }
+};
+
